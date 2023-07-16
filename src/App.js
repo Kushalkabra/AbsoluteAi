ba84514af705e52a6190da43bc56d77d24a25700
unknown <kushkabra23@gmail.com>
2023-07-16 16:21:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import Homepage from "./Components/Homepage/Homepage";
+import Header from "./Components/Header/Header";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+  BrowserRouter
+} from "react-router-dom";
+
+import Writing from "./Pages/Writing";
+import Social from "./Pages/Social";
+import Marketing from "./Pages/Marketing";
+import Seo from "./Pages/Seo";
+import Design from "./Pages/Design";
+import Travel from "./Pages/Travel";
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Main_app" >
+      <Header/>
+      <BrowserRouter>
+      {/* <Navbar/> */}
+      {/* <Test/> */}
+      <Routes>
+      <Route path='/' element={<Homepage/>}></Route>
+      <Route path='/Social' element={<Social/>}></Route>
+      <Route path='/Writing' element={<Writing/>}></Route>
+      <Route path='/Marketing' element={<Marketing/>}></Route>
+      <Route path='/Seo' element={<Seo/>}></Route>
+      <Route path='/Design' element={<Design/>}></Route>
+      <Route path='/Travel' element={<Travel/>}></Route>
+      </Routes>
+      </BrowserRouter>
+    
+      {/* <Homepage/> */}
+    
     </div>
   );
 }
